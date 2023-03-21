6929e18119777df087da59a5518b38284643d9a7
Yoshioka <sss.yoshioka@gmail.com>
2023-03-21 13:00:19 +0000

    🎸 feat: AdminUser 作成

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_115224) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_125321) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admin_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.boolean "activated", null: false
