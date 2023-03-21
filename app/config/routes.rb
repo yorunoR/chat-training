b067d0514358bd6ab9b5f90963d06031d231aacf
Yoshioka <sss.yoshioka@gmail.com>
2023-03-21 12:05:00 +0000

    🎸 feat: GraphQL 導入

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :admin_users
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+  post "/graphql", to: "graphql#execute"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
