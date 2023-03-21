ed9dd56059c7d651c1f583911b41a91ce2b52dce
Yoshioka <sss.yoshioka@gmail.com>
2023-03-21 14:22:25 +0000

    🎸 feat: PrimeVue 設定

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,22 +1,46 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+// import BoardLayout from '@/layouts/BoardLayout.vue'
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      redirect: '/ping'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/',
+      component: DefaultLayout,
+      children: [
+        {
+          path: 'ping',
+          name: 'ping',
+          component: async () => await import(/* webpackChunkName: "ping" */ '@/views/PingView.vue')
+        },
+        {
+          path: 'sign_in',
+          name: 'sign_in',
+          component: async () =>
+            await import(/* webpackChunkName: "sign_in" */ '@/views/SignInView.vue')
+        }
+      ]
     }
+    // {
+    //   path: '/board',
+    //   component: BoardLayout,
+    //   children: [
+    //     {
+    //       path: 'question',
+    //       name: 'question',
+    //       component: async () =>
+    //         await import(
+    //           /* webpackChunkName: "question" */ '@/views/board/QuestionView.vue'
+    //         )
+    //     }
+    //   ]
+    // }
   ]
 })
 
---
+++ web/src/views/PingView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>This is a ping page</h1>
+    <Button>PrimeVue Valid</Button>
+  </div>
+</template>
