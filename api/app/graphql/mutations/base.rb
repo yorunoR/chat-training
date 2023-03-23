dc99c9bd7452fa4f7a0274f7b7b9ffe58259af47
Yoshioka <sss.yoshioka@gmail.com>
2023-03-23 12:01:35 +0000

    🐛 fix: アクセス制限修正

--- api/app/graphql/mutations/base.rb
+++ api/app/graphql/mutations/base.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 module Mutations
   class Base < Mutations::BaseMutation
-    def authorized?
+    def authorized?(_args)
       raise GraphQL::ExecutionError, 'login required!!' unless context[:current_user]
+
+      true
     end
   end
 end
