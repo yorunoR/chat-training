294f1517e4aa829707b7b997e8e55dd1410a2bd2
Yoshioka <sss.yoshioka@gmail.com>
2023-03-24 10:22:45 +0000

    🎸 feat: ChatGPT モデル一覧取得

--- api/app/services/openai_client.rb
+++ api/app/services/openai_client.rb
@@ -21,4 +21,16 @@ class OpenaiClient
 
     response.dig('choices', 0, 'message', 'content')
   end
+
+  def model_list
+    models = @client.models.list
+    models['data'].map do |model|
+      p model
+      {
+        id: model['id'],
+        owned_by: model['owned_by'],
+        root: model['root']
+      }
+    end
+  end
 end
